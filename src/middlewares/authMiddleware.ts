3bf6a831699bafffbe4c234fee1e56c69804718b
guilhermescr <guilhermechaves2005@hotmail.com>
2025-08-17 00:26:12 -0300

    feat: enhance auth middleware to support debug auto-login using env

--- src/middlewares/authMiddleware.ts
+++ src/middlewares/authMiddleware.ts
@@ -2,12 +2,22 @@ import { Response, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
 import { AuthenticatedRequest } from '../types/auth';
 import { AuthPayloadDto } from '../dtos/auth.dto';
+import mongoose from 'mongoose';
 
 export function authMiddleware(
   req: AuthenticatedRequest,
   res: Response,
   next: NextFunction
 ) {
+  if (process.env.DEBUG_LOGIN === 'true') {
+    req.user = {
+      email: process.env.DEBUG_EMAIL || 'debug@debug.com',
+      id: new mongoose.Types.ObjectId().toString(),
+      username: process.env.DEBUG_USERNAME || 'DEBUG_USERNAME',
+    };
+    return next();
+  }
+
   const authHeader = req.headers.authorization;
   if (!authHeader || !authHeader.startsWith('Bearer ')) {
     return next({
